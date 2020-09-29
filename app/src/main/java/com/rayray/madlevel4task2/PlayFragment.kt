3e4c8939cecb4e9b09d0c187cbd9eee515d2cf00
raymondbdws <raymondbdws@hotmail.com>
2020-09-29 17:17:17 +0200

    layout is af

--- app/src/main/java/com/rayray/madlevel4task2/PlayFragment.kt
+++ app/src/main/java/com/rayray/madlevel4task2/PlayFragment.kt
@@ -18,14 +18,14 @@ class PlayFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_play, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 }
